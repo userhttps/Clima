3f46637201642b34314854147be87108a2160dcf
userhttps <nicolasferreiragit@gmail.com>
2025-06-23 15:55:41 -0300

    Update post.php

--- post.php
+++ post.php
@@ -1,23 +1,18 @@
 <?php
-// URL do seu Firebase - coloque o final .json para pegar o JSON
-$url = "https://climaconsciente-131f2-default-rtdb.firebaseio.com/sensor.json";
+$temp = $_POST['temp'] ?? null;
+$umid = $_POST['umid'] ?? null;
+$press = $_POST['press'] ?? null;
+$heat = $_POST['heat'] ?? null;
 
-// Pega o conteúdo JSON da URL
-$json = file_get_contents($url);
+$data = [
+  "temperatura" => $temp,
+  "umidade" => $umid,
+  "pressao" => $press,
+  "sensacao_termica" => $heat,
+  "hora" => date("H:i:s"),
+  "data" => date("Y-m-d")
+];
 
-// Converte JSON em array PHP
-$data = json_decode($json, true);
-
-// Exibe os dados
-echo "<h1>Dados do Sensor</h1>";
-if ($data) {
-    echo "Temperatura: " . htmlspecialchars($data['temperatura']) . " °C<br>";
-    echo "Umidade: " . htmlspecialchars($data['umidade']) . " %<br>";
-    echo "Pressão: " . htmlspecialchars($data['pressao']) . " hPa<br>";
-    if (isset($data['sensacao_termica'])) {
-        echo "Sensação Térmica: " . htmlspecialchars($data['sensacao_termica']) . " °C<br>";
-    }
-} else {
-    echo "Não foi possível obter os dados do Firebase.";
-}
+file_put_contents("dados.json", json_encode($data));
+echo "OK";
 ?>
